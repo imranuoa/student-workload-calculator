c0c006512ba10d1169341db637eaaa3e8b1a2c19
Zac M-W <z.millerwaugh@auckland.ac.nz>
2022-09-30 12:04:52 +1300

    Add description to add component feature

--- src/lib/add-activity/addActivity.svelte
+++ src/lib/add-activity/addActivity.svelte
@@ -1,43 +1,100 @@
 <script lang="ts">
-	import { slide } from 'svelte/transition';
+	import { fade, fly, slide } from 'svelte/transition';
 	import { createEventDispatcher } from 'svelte';
 	import type { ActivitySubClass } from '../activities';
 	import { activeCourse, courses } from '../../store';
+	import { writable } from 'svelte/store';
 
 	$: courseMeta = $activeCourse >= 0 ? $courses[$activeCourse].meta : undefined;
 
 	const dispatch = createEventDispatcher();
 
 	export let activities: ActivitySubClass[];
+
+	let hoveredActivity = -1;
+	let hoveredTimeout: number;
 </script>
 
-<div class="activity-grid mb-5" transition:slide>
-	{#each activities as activity, i}
-		<button class="btn" on:click={() => dispatch('add', activity)}>
-			<span class="icon">{activity.icon}</span>
-			<span class="label">{activity.label}</span>
-		</button>
-	{/each}
+<div
+	on:mouseleave={() => {
+		hoveredTimeout = window.setTimeout(() => {
+			hoveredActivity = -1;
+		}, 300);
+	}}
+	class="activityWrapper"
+>
+	<div class="activity-grid mb-5" transition:slide>
+		{#each activities as activity, i}
+			<button
+				class="btn"
+				on:click={() => dispatch('add', activity)}
+				on:mouseenter={() => {
+					clearTimeout(hoveredTimeout);
+					hoveredActivity = i;
+				}}
+			>
+				<span class="icon">{activity.icon}</span>
+				<span class="label">{activity.label}</span>
+			</button>
+		{/each}
+	</div>
+	<div class="activity-info-wrapper">
+		{#if hoveredActivity >= 0}
+			{#key hoveredActivity}
+				<div
+					class="activity-info"
+					transition:fly={{ y: 10 }}
+					on:mouseenter={() => {
+						clearTimeout(hoveredTimeout);
+					}}
+				>
+					<h3>
+						{activities[hoveredActivity].icon}
+						{activities[hoveredActivity].label}
+					</h3>
+					<p>{activities[hoveredActivity].description}</p>
+				</div>
+			{/key}
+		{/if}
+	</div>
 </div>
 
 <style lang="postcss">
-	.activity-grid {
-		@apply grid grid-cols-5 gap-2;
-		.btn {
-			/* Default */
-			@apply flex flex-col items-center justify-center w-full relative rounded-lg  outline-blue-400 outline-0 outline overflow-hidden bg-gray-50 text-black;
-			/* On Hover */
-			@apply transition hover:shadow-md hover:outline-2;
-			.icon {
-				@apply text-2xl;
-			}
-			.label {
-				@apply text-xs;
-				/* @apply text-xs absolute w-full h-full opacity-0 transition bg-gray-100 flex items-center; */
-			}
-			&:hover {
+	.activityWrapper {
+		@apply relative;
+		.activity-grid {
+			@apply grid grid-cols-5 gap-2;
+			.btn {
+				/* Default */
+				@apply flex flex-col items-center justify-center w-full relative rounded-lg  outline-blue-400 outline-0 outline overflow-hidden bg-gray-50 text-black;
+				/* On Hover */
+				@apply transition hover:shadow-md hover:outline-2;
+				.icon {
+					@apply text-2xl;
+				}
 				.label {
-					@apply opacity-100;
+					@apply text-xs;
+					/* @apply text-xs absolute w-full h-full opacity-0 transition bg-gray-100 flex items-center; */
+				}
+				&:hover {
+					.label {
+						@apply opacity-100;
+					}
+				}
+			}
+		}
+
+		.activity-info-wrapper {
+			@apply grid absolute;
+			grid-template-areas: 'content';
+			.activity-info {
+				@apply m-3 p-4 rounded-lg shadow-md  bg-white;
+				grid-area: content;
+				h3 {
+					@apply text-xl text-center;
+				}
+				p {
+					@apply text-sm;
 				}
 			}
 		}
